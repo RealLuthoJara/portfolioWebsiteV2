893df3b4c2fa179b74229ea9b927ea36daa5c7f9
LuthoJara <Luthojaraofficial@gmail.com>
2024-04-30 05:56:47 +0200

    Created & completed about section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Sidebar from './Components/Sidebar/Sidebar'
 import Home from './Components/Home/Home'
-
+import About from './Components/About/About'
 const App = () => {
   return (
     <>
     
-    <Sidebar/>
-    {/* <Home/> */}
+    <Sidebar />
+    <About />   
+     
     </>
   )
 }

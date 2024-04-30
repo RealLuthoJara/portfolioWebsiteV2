8b2c0bbda97af75aab992a5ff00d503aa34d9547
LuthoJara <Luthojaraofficial@gmail.com>
2024-04-30 05:09:13 +0200

    restoring files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './Components/Sidebar/Sidebar'
+import Home from './Components/Home/Home'
+
+const App = () => {
+  return (
+    <>
+    
+    <Sidebar/>
+    {/* <Home/> */}
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./Sidebar.css";
+
+import Home from "../Home/Home";
+
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+
+import SidebarList from "./SidebarList";
+
+const Sidebar = () => {
+  const [expandSidebar, setExpandSidebar] = useState(true);
+
+  const toggleExpand = () => {
+    setExpandSidebar(!expandSidebar);
+  };
+
+  return (
+    <div className="container-fluid sidebar-section">
+      <div className={expandSidebar ? "sidebar-expand sidebar" : "sidebar"}>
+        <div className="sidebar-expand-and-collapse">
+          <p onClick={toggleExpand}>
+            {expandSidebar ? (
+              <BsChevronLeft size={30} />
+            ) : (
+              <BsChevronRight size={30} />
+            )}
+          </p>
+        </div>
+
+        <SidebarList expandSidebar={expandSidebar} />
+      </div>
+
+      <div className="container-fluid">
+        <Home />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;

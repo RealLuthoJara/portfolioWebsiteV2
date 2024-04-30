d01614880233453633abc681cf0cfe8acfeb9770
LuthoJara <Luthojaraofficial@gmail.com>
2024-04-30 06:53:12 +0200

    Added a description

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -19,9 +19,10 @@ const About = () => {
           <div className="about-details">
             <h4>About Me</h4>
             <span className="underline"></span>
-            <p>
-              About Section  
-              It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            <p>   
+              With five years of programming experience, I bring a diverse skill set and eagerness to excel. 
+              Proficient in problem-solving, communication, and coordination, I strive for optimal solutions. My expertise spans copywriting, sales, marketing, lead generation, graphic and web design, and programming. 
+              Articulate and empathetic, I effectively understand and address user needs and pain points. Known for attention to detail and a commitment to intuitive user experiences, I thrive in fast-paced environments with tight deadlines.            
             </p>
           </div>  
         </div>

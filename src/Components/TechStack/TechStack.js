6552b8775c2c20b7d7242d42d6d087ca8c50ca54
LuthoJara <Luthojaraofficial@gmail.com>
2024-04-30 21:09:06 +0200

    expanding techstack tile

--- src/Components/TechStack/TechStack.js
+++ src/Components/TechStack/TechStack.js
@@ -1,35 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TechStack.css'
 
 const TechStack = () => {
+    const [expandTechStack, setExpandTechStack] = useState(false);
+
+    const toggleExpand = (isMouseOver) => {
+        setExpandTechStack(isMouseOver);
+      };
+    
     const data = [
         {
-            name: 'HTML'
+            symbol: 'n',
+            name: 'HTML',
+            projects: 'kh', 
+            color: 'omodo',
+            backgroundColor: '#000000',
         },
-        {
-            name: 'CSS'
-        },
-        {
-            name: 'JavaScript'
-        },
-        {
-            name: 'React'
-        },
-        {
-            name: 'Figma'
-        },
-        {
-            name: 'BootStrap'
-        },
-        {
-            name: 'Tailwind'
-        },
-        {
-            name: 'Material UI'
-        },
-        {
-            name: 'Jest'
-        } 
+        // {
+        //     symbol: n,
+        //     name: 'CSS',
+        //     projects: kh, 
+        //     color: omodo,
+        //     backgroundColor: elivr,
+        // },
+        // {
+        //     symbol: n,
+        //     name: 'JavaScript',
+        //     projects: kh, 
+        //     color: omodo,
+        //     backgroundColor: elivr,
+        // },
+        // {
+        //     symbol: n,
+        //     name: 'React',
+        //     projects: kh, 
+        //     color: omodo,
+        //     backgroundColor: elivr,        
+        // },
+        // {
+        //     symbol: n,
+        //     name: 'Figma',
+        //     projects: kh, 
+        //     color: omodo,
+        //     backgroundColor: elivr,        
+        // },
+        // {
+        //     symbol: n,
+        //     name: 'Bootstrap',
+        //     projects: kh, 
+        //     color: omodo,
+        //     backgroundColor: elivr,
+        // },
+        // {
+        //     symbol: n,
+        //     name: 'Tailwind',
+        //     projects: kh, 
+        //     color: omodo,
+        //     backgroundColor: elivr,
+        // },
+        // {
+        //     symbol: n,
+        //     name: 'Material UI',
+        //     projects: kh, 
+        //     color: omodo,
+        //     backgroundColor: elivr,        
+        // }, 
     ]
   return (
     <div className='container techstack-section'>
@@ -42,8 +77,22 @@ const TechStack = () => {
         <div className='row container'>
             {data.map((item, index)=>(
                 <div className='col-xl-4 col-lg-6 col-md-6 col-sm-12'>
-                    <div className='tech-content'>
-                        <p>{item.name}</p>
+                    <div onMouseOver={() => toggleExpand(true)} onMouseOut={() => toggleExpand(false)} className={!expandTechStack ? 'tech-content-expand tech-content' : 'tech-content'}>
+
+                    {/* <div onMouseOver={toggleExpand} className= {!expandTechStack ? '.tech-content-expand tech-content' : 'tech-content'}> */}
+                        {!expandTechStack ? (
+                        <p>{item.name}
+                        </p>
+                        ) : (
+                        <p>
+                            {item.symbol}
+                            {item.name}
+                            {item.projects}
+                            {item.color}
+                            {item.backgroundColor}
+                        </p>
+
+                        ) }
                     </div>
                 </div>
             ))}

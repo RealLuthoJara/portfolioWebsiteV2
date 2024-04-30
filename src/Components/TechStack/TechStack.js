d0d9c14e157f693a73aeddca6ef476277e6f7027
LuthoJara <Luthojaraofficial@gmail.com>
2024-04-30 06:54:01 +0200

    Created tech stack

---
+++ src/Components/TechStack/TechStack.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import './TechStack.css'
+
+const TechStack = () => {
+    const data = [
+        {
+            name: 'HTML'
+        },
+        {
+            name: 'CSS'
+        },
+        {
+            name: 'JavaScript'
+        },
+        {
+            name: 'React'
+        },
+        {
+            name: 'Figma'
+        },
+        {
+            name: 'BootStrap'
+        },
+        {
+            name: 'Tailwind'
+        },
+        {
+            name: 'Material UI'
+        },
+        {
+            name: 'Jest'
+        } 
+    ]
+  return (
+    <div className='container techstack-section'>
+
+        <div className='section-title'>
+            <h4>Tech Stack</h4>
+            <span className="underline"></span>
+        </div>
+        
+        <div className='row'>
+            {data.map((item, index)=>(
+                <div className='w-100 col-xl-12 col-lg-6 col-md-6 col-sm-12'>
+                    <div className='tech-content'>
+                        <p>{item.name}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default TechStack

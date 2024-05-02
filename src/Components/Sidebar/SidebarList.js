5bd879879fa818053f5333eb97d146bbda3a454d
LuthoJara <Luthojaraofficial@gmail.com>
2024-05-02 21:14:28 +0200

    made the sizes better

--- src/Components/Sidebar/SidebarList.js
+++ src/Components/Sidebar/SidebarList.js
@@ -16,14 +16,14 @@ const SidebarList = ({expandSidebar}) => {
                     <img src={profilepic} alt='Profile Picture'/>
                 </div>
                 <ul>
-                    <li className='nav-item'><FcHome size={25}/>Home</li>
-                    <li className='nav-item'><FcBusinessman size={25}/>About Me</li>
-                    <li className='nav-item'><FcBullish size={25}/>Work Experience</li>
-                    <li className='nav-item'><FcGenealogy size={25}/>Tech Stack</li>
-                    <li className='nav-item'><FcGraduationCap size={25}/>Education</li>
-                    <li className='nav-item'><FcDatabase size={25}/>Projects</li>
-                    <li className='nav-item'><FcRating size={25}/>Testimonials</li>
-                    <li className='nav-item'><FcContacts size={25}/>Contact Me</li>
+                    <li className='nav-item'><FcHome size={20}/>Home</li>
+                    <li className='nav-item'><FcBusinessman size={20}/>About Me</li>
+                    <li className='nav-item'><FcBullish size={20}/>Work Experience</li>
+                    <li className='nav-item'><FcGenealogy size={20}/>Tech Stack</li>
+                    <li className='nav-item'><FcGraduationCap size={20}/>Education</li>
+                    <li className='nav-item'><FcDatabase size={20}/>Projects</li>
+                    <li className='nav-item'><FcRating size={20}/>Testimonials</li>
+                    <li className='nav-item'><FcContacts size={20}/>Contact Me</li>
                 </ul>
             </div>
         ) : (
--- src/Components/TechStack/TechStack.js
+++ src/Components/TechStack/TechStack.js
@@ -3,7 +3,7 @@ import './TechStack.css'
 
 import html from './Symbols/html.png';
 import css from './Symbols/css.png';
-import js from './Symbols/js.png';
+import js from './Symbols/js.svg';
 import react from './Symbols/react.png';
 import angular from './Symbols/angular.png';
 import bootstrap from './Symbols/bootstrap.png';
@@ -34,7 +34,9 @@ const TechStack = () => {
             name: 'HTML',
             projects: 'kh', 
             color: 'omodo',
-            backgroundColor: '#ffffff',
+            backgroundColor: 'elivr',
+//             background: rgb(213,127,35);
+// background: linear-gradient(94deg, rgba(213,127,35,1) 25%, rgba(205,21,21,1) 75%);
         },
         {
             symbol: css,
@@ -42,6 +44,8 @@ const TechStack = () => {
             projects: 'kh', 
             color: 'omodo',
             backgroundColor: 'elivr',
+//             background: rgb(35,213,194);
+// background: linear-gradient(94deg, rgba(35,213,194,1) 25%, rgba(21,39,205,1) 75%);
         },
         {
             symbol: js,
@@ -49,13 +53,17 @@ const TechStack = () => {
             projects: 'kh', 
             color: 'omodo',
             backgroundColor: 'elivr',
+//             background: rgb(208,213,35);
+// background: linear-gradient(94deg, rgba(208,213,35,1) 25%, rgba(0,0,0,1) 75%);
         },
         {
             symbol: react,
             name: 'React',
             projects: 'kh', 
             color: 'omodo',
-            backgroundColor: 'elivr',        
+            backgroundColor: 'elivr',   
+//             background: rgb(141,34,172);
+// background: linear-gradient(47deg, rgba(141,34,172,1) 20%, rgba(18,53,150,1) 40%, rgba(20,185,172,1) 69%);     
         },
         {
             symbol: figma,
@@ -63,6 +71,8 @@ const TechStack = () => {
             projects: 'kh', 
             color: 'omodo',
             backgroundColor: 'elivr',        
+//             background: rgb(34,172,50);
+// background: linear-gradient(90deg, rgba(34,172,50,1) 10%, rgba(114,18,150,1) 31%, rgba(26,206,192,1) 47%, rgba(223,68,28,1) 66%, rgba(218,111,56,1) 87%, rgba(218,111,56,1) 87%);
         },
         {
             symbol: bootstrap,
@@ -70,6 +80,8 @@ const TechStack = () => {
             projects: 'kh', 
             color: 'omodo',
             backgroundColor: 'elivr',
+//             background: rgb(114,18,150);
+// background: radial-gradient(circle, rgba(114,18,150,1) 51%, rgba(255,255,255,1) 100%);
         },
         {
             symbol: tailwind,
@@ -77,13 +89,17 @@ const TechStack = () => {
             projects: 'kh', 
             color: 'omodo',
             backgroundColor: 'elivr',
+//             background: rgb(18,144,150);
+// background: linear-gradient(90deg, rgba(18,144,150,1) 51%, rgba(17,185,110,1) 100%);
         },
         {
             symbol: mui,
             name: 'Material UI',
             projects: 'kh', 
             color: 'omodo',
-            backgroundColor: 'elivr',        
+            backgroundColor: 'elivr', 
+//             background: rgb(18,42,150);
+// background: linear-gradient(90deg, rgba(18,42,150,1) 45%, rgba(17,185,173,1) 92%);       
         }, 
     ]
   return (
@@ -96,19 +112,24 @@ const TechStack = () => {
         
         <div className='row container'>
             {data.map((item, index)=>(
-                <div className='col-xl-4 col-lg-6 col-md-6 col-sm-12'>
-                    <div onMouseOver={() => toggleExpand(true)} onMouseOut={() => toggleExpand(false)} className={!expandTechStack ? 'tech-content' : 'tech-content tech-content-expand'}>
+                <div className='col-xl-4 col-lg-6 col-md-6 col-sm-12' key={index}>
+                    <div 
+                        onMouseOver={()=> toggleExpand(true)} onMouseOut={() => toggleExpand(false)} 
+                        className={!expandTechStack ? 'tech-content' : 'tech-content tech-content-expand'} 
+                        style={!expandTechStack ? { backgroundColor: item.backgroundColor } : {}}
+                        key={index}
+                    >
 
                     {/* <div onMouseOver={toggleExpand} className= {!expandTechStack ? '.tech-content-expand tech-content' : 'tech-content'}> */}
                         {!expandTechStack ? (
-                        <p>
+                        <span>
                             <div className='tech-stack-symbol'>
                                 <img src={item.symbol} alt='Tech Stack Symbol'/>
                             </div>
                             {item.name}
-                        </p>
-                        ) : (
-                        <p>
+                        </span>
+                        ) : ( 
+                        <span>
                             <div className='tech-stack-symbol'>
                                 <img src={item.symbol} alt='Tech Stack Symbol'/>
                             </div>
@@ -116,13 +137,16 @@ const TechStack = () => {
                             {item.projects}
                             {item.color}
                             {item.backgroundColor}
-                        </p>
+                        </span>
 
                         ) }
                     </div>
                 </div>
             ))}
         </div>
+        <span className='load-more'>
+            Load More
+        </span>
     </div>
   )
 }

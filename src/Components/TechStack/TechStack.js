8682ac697eb216dc01f4f8ca4e0ecc68df7c455a
LuthoJara <Luthojaraofficial@gmail.com>
2024-05-06 17:58:33 +0200

    Ensured that when I click, it opens up one at a time.

--- src/Components/TechStack/TechStack.js
+++ src/Components/TechStack/TechStack.js
@@ -17,91 +17,119 @@ import mui from './Symbols/mui.png';
 import mysql from './Symbols/mysql.png';
 import npm from './Symbols/npm.png';
 import styledComponents from './Symbols/styled-components.png';
-import tailwind from './Symbols/tailwind.jpeg';
+import tailwind from './Symbols/tailwind.jpg';
 import typescript from './Symbols/typescript.png';
 import wordpress from './Symbols/wordpress.png';
 
 const TechStack = () => {
-    const [expandTechStack, setExpandTechStack] = useState(false);
 
-    const toggleExpand = (isMouseOver) => {
-        setExpandTechStack(isMouseOver);
-      };
+    // Function to toggle selected tech index
+    // const toggleInfo = (index) => {
+    //     if (selectedTechIndex === index) {
+    //     // If already selected, deselect
+    //     setSelectedTechIndex(null);
+    //     } else {
+    //     // Otherwise, select
+    //     setSelectedTechIndex(index);
+    //     }
+    // };
     
-    const data = [
+    
+    //The data for each stack
+    const techStackData = [
         {
             symbol: html,
-            name: 'HTML',
-            projects: 'kh', 
-            color: 'omodo',
-            backgroundColor: 'elivr',
-//             background: rgb(213,127,35);
-// background: linear-gradient(94deg, rgba(213,127,35,1) 25%, rgba(205,21,21,1) 75%);
+            name: 'HTML', 
+            color: 'white', 
+            projects: ['links'],
+            background: 'linear-gradient(94deg, rgba(213,127,35,1) 25%, rgba(205,21,21,1) 75%)',
         },
         {
             symbol: css,
-            name: 'CSS',
-            projects: 'kh', 
-            color: 'omodo',
-            backgroundColor: 'elivr',
-//             background: rgb(35,213,194);
-// background: linear-gradient(94deg, rgba(35,213,194,1) 25%, rgba(21,39,205,1) 75%);
+            name: 'CSS', 
+            color: 'white',
+            projects: ['links'], 
+            background: 'linear-gradient(94deg, rgba(35,213,194,1) 25%, rgba(21,39,205,1) 75%)',
         },
         {
             symbol: js,
-            name: 'JavaScript',
-            projects: 'kh', 
-            color: 'omodo',
-            backgroundColor: 'elivr',
-//             background: rgb(208,213,35);
-// background: linear-gradient(94deg, rgba(208,213,35,1) 25%, rgba(0,0,0,1) 75%);
+            name: 'JavaScript', 
+            color: 'black',
+            projects: ['links'], 
+            background: 'linear-gradient(94deg, rgba(208,213,35,1) 25%, rgba(255,255,255,1) 75%)',
         },
         {
             symbol: react,
-            name: 'React',
-            projects: 'kh', 
-            color: 'omodo',
-            backgroundColor: 'elivr',   
-//             background: rgb(141,34,172);
-// background: linear-gradient(47deg, rgba(141,34,172,1) 20%, rgba(18,53,150,1) 40%, rgba(20,185,172,1) 69%);     
+            name: 'React', 
+            color: 'white',
+            projects: ['links'],    
+            background: 'linear-gradient(47deg, rgba(141,34,172,1) 20%, rgba(18,53,150,1) 40%, rgba(20,185,172,1) 69%)',     
         },
         {
             symbol: figma,
-            name: 'Figma',
-            projects: 'kh', 
-            color: 'omodo',
-            backgroundColor: 'elivr',        
-//             background: rgb(34,172,50);
-// background: linear-gradient(90deg, rgba(34,172,50,1) 10%, rgba(114,18,150,1) 31%, rgba(26,206,192,1) 47%, rgba(223,68,28,1) 66%, rgba(218,111,56,1) 87%, rgba(218,111,56,1) 87%);
+            name: 'Figma', 
+            color: 'white',
+            projects: ['links'],        
+            background: 'linear-gradient(90deg, rgba(34,172,50,1) 10%, rgba(114,18,150,1) 31%, rgba(223,68,28,1) 47%, rgba(218,111,56,1) 66%, rgba(218,111,56,1) 87%, rgba(26,206,192,1) 87%)',
         },
         {
             symbol: bootstrap,
-            name: 'Bootstrap',
-            projects: 'kh', 
-            color: 'omodo',
-            backgroundColor: 'elivr',
-//             background: rgb(114,18,150);
-// background: radial-gradient(circle, rgba(114,18,150,1) 51%, rgba(255,255,255,1) 100%);
+            name: 'Bootstrap', 
+            color: 'white',
+            projects: ['links'], 
+            background: 'radial-gradient(circle, rgba(114,18,150,1) 51%, rgba(255,255,255,1) 100%)',
         },
         {
             symbol: tailwind,
-            name: 'Tailwind',
-            projects: 'kh', 
-            color: 'omodo',
-            backgroundColor: 'elivr',
-//             background: rgb(18,144,150);
-// background: linear-gradient(90deg, rgba(18,144,150,1) 51%, rgba(17,185,110,1) 100%);
+            name: 'Tailwind', 
+            color: 'white',
+            projects: ['links'], 
+            background: 'linear-gradient(90deg, rgba(18,144,150,1) 51%, rgba(17,185,110,1) 100%)',
         },
         {
             symbol: mui,
-            name: 'Material UI',
-            projects: 'kh', 
-            color: 'omodo',
-            backgroundColor: 'elivr', 
-//             background: rgb(18,42,150);
-// background: linear-gradient(90deg, rgba(18,42,150,1) 45%, rgba(17,185,173,1) 92%);       
-        }, 
+            name: 'Material UI', 
+            color: 'white',
+            projects: ['links'],  
+            background: 'linear-gradient(90deg, rgba(18,42,150,1) 45%, rgba(17,185,173,1) 92%)',        
+    }, 
     ]
+
+    // This is so that only one expands when I click on the info
+    // const [expandedItems, setExpandedItems] = useState([]);
+    
+    const [expandedIndex, setExpandedIndex] = useState(null);
+
+    // const toggleExpand = (index) => {
+    //     const newExpandedItems = [...expandedItems];
+    //     newExpandedItems[index] = !newExpandedItems[index];
+    //     setExpandedItems(newExpandedItems);
+    // };
+
+    const toggleExpand = (index) => {
+        if (expandedIndex === index) {
+            // Collapse if already expanded
+            setExpandedIndex(null); 
+        } else {
+            // Expand clicked item
+            setExpandedIndex(index); 
+        }
+    };
+
+    // True or false if it currently expanded
+    // const [expandTechStack, setExpandTechStack] = useState(false);
+
+    // const expandTech = () => {
+    //     setExpandTechStack(!expandTechStack);
+    // };
+
+    // To show more teck stack projects
+    const [showMoreTech, setShowMoreTech] = useState(3);
+
+    const loadMore = ()=>{
+        setShowMoreTech((prev)=>prev+3)
+    }
+
   return (
     <div className='container tech-stack-section'>
 
@@ -109,34 +137,41 @@ const TechStack = () => {
             <h4>Tech Stack</h4>
             <span className="underline"></span>
         </div>
-        
+
         <div className='row container'>
-            {data.map((item, index)=>(
+
+            {/* This (techStackData.slice(0, showMoreTech)) is to show that only the 1st three items should appear */}
+            {techStackData.slice(0, showMoreTech).map((item, index)=>(
                 <div className='col-xl-4 col-lg-6 col-md-6 col-sm-12' key={index}>
                     <div 
-                        onMouseOver={()=> toggleExpand(true)} onMouseOut={() => toggleExpand(false)} 
-                        className={!expandTechStack ? 'tech-content' : 'tech-content tech-content-expand'} 
-                        style={!expandTechStack ? { backgroundColor: item.backgroundColor } : {}}
-                        key={index}
+                        className={expandedIndex === index ? 'tech-content tech-content-expand' : 'tech-content'} 
+                        style={
+                            expandedIndex === index ? { 
+                            background: item.background, 
+                            color: item.color, 
+                        } : {}}
+                        onClick={() => {
+                            toggleExpand(index);
+                        }}                  
                     >
-
-                    {/* <div onMouseOver={toggleExpand} className= {!expandTechStack ? '.tech-content-expand tech-content' : 'tech-content'}> */}
-                        {!expandTechStack ? (
+                        {expandedIndex === index ? (
                         <span>
                             <div className='tech-stack-symbol'>
                                 <img src={item.symbol} alt='Tech Stack Symbol'/>
                             </div>
-                            {item.name}
+                            <div>{item.name}</div>
+                            {expandedIndex === index && (
+                            <div>
+                                <div>{item.projects}</div>
+                            </div>
+                        )}
                         </span>
                         ) : ( 
                         <span>
                             <div className='tech-stack-symbol'>
                                 <img src={item.symbol} alt='Tech Stack Symbol'/>
                             </div>
-                            {item.name}
-                            {item.projects}
-                            {item.color}
-                            {item.backgroundColor}
+                            <div>{item.name}</div>
                         </span>
 
                         ) }
@@ -144,9 +179,11 @@ const TechStack = () => {
                 </div>
             ))}
         </div>
-        <span className='load-more'>
-            Load More
-        </span>
+        {showMoreTech >= techStackData.length ? null : (
+            <div className='load-more-items' onClick={loadMore}>
+                Load More
+            </div>
+        )}
     </div>
   )
 }

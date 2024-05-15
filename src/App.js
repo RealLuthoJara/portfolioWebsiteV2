97e7470a66968c9058553f30804fc5dcd514dd5e
LuthoJara <Luthojaraofficial@gmail.com>
2024-05-15 14:34:33 +0200

    added react link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+
 import Sidebar from './Components/Sidebar/Sidebar'
 import About from './Components/About/About'
 import TechStack from './Components/TechStack/TechStack'
 import Project from './Components/Projects/Project'
-
+import WorkExperience from './Components/WorkExperience/WorkExperience'
+import Contact from './Components/Contact/Contact'
 
 const App = () => {
   return (
@@ -11,8 +13,10 @@ const App = () => {
     
     <Sidebar />
     <About />   
+    <WorkExperience />
     <TechStack />
     <Project />
+    <Contact />
 
     </>
   )
